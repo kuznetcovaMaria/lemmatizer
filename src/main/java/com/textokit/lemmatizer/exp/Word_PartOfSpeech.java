3b03c9b081f4d10f8a92cce172277112b2c37814
kuznetcovaMaria <mari13061992@mail.ru>
2015-08-21 12:37:36 +0300

    Структура данных

--- src/main/java/com/textokit/lemmatizer/exp/Word_PartOfSpeech.java
+++ src/main/java/com/textokit/lemmatizer/exp/Word_PartOfSpeech.java
@@ -1,33 +1,49 @@
 package com.textokit.lemmatizer.exp;
 
 import org.opencorpora.cas.Word;
-
+/**
+ * Структура для хранения словоформы, ее части речи и леммы
+ *
+ */
 public class Word_PartOfSpeech {
+	private String wordString;
 	private Word word;
 	private String partOfSpeech;
+	private String lemma;
+
+	public Word_PartOfSpeech(String w, String p, String l) {
+		wordString = w;
+		partOfSpeech = p;
+		lemma = l;
+	}
 
 	public Word_PartOfSpeech(Word w, String p) {
 		word = w;
+		wordString = word.getCoveredText();
 		partOfSpeech = p;
+		lemma = "";
 	}
 
 	public Word_PartOfSpeech() {
 		word = null;
 		partOfSpeech = "";
-	}
-
-	@Override
-	public String toString() {
-		return "Word_PartOfSpeech [word=" + word + ", partOfSpeech="
-				+ partOfSpeech + "]";
+		wordString = "";
+		lemma = "";
 	}
 
 	public Word getWord() {
 		return word;
 	}
 
+	@Override
+	public String toString() {
+		return "Word_PartOfSpeech [wordString=" + wordString
+				+ ", partOfSpeech=" + partOfSpeech + ", lemma=" + lemma + "]";
+	}
+
 	public void setWord(Word w) {
 		word = w;
+		wordString = word.getCoveredText();
 	}
 
 	public String getPartOfSpeech() {
@@ -38,40 +54,46 @@ public class Word_PartOfSpeech {
 		partOfSpeech = s;
 	}
 
+	public String getLemma() {
+		return lemma;
+	}
+
+	public void setLemma(String l) {
+		lemma = l;
+	}
+
+	public String getStringWord() {
+		return wordString;
+	}
+
+	public void setStringWord(String w) {
+		wordString = w;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
 				+ ((partOfSpeech == null) ? 0 : partOfSpeech.hashCode());
-		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		result = prime * result
+				+ ((wordString == null) ? 0 : wordString.hashCode());
 		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Word_PartOfSpeech other = (Word_PartOfSpeech) obj;
-		if (partOfSpeech == null) {
-			if (other.partOfSpeech != null)
-				return false;
-		} else if (!partOfSpeech.equals(other.partOfSpeech))
-			return false;
-		if (word == null) {
-			if (other.word != null)
-				return false;
-		} else if (!word.equals(other.word))
-			return false;
-		return true;
+	public boolean equals(Word_PartOfSpeech str) {
+
+		if (wordString.equals(str.getStringWord())) {
+			if (partOfSpeech.equals(str.getPartOfSpeech())) {
+				if (lemma.equals(str.getLemma())) {
+					// System.out.println("true");
+					return true;
+				}
+			}
+		}
+
+		return false;
+
 	}
 
-
-
-	
-	
 }
